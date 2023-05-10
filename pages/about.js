3225ac06d473427758dd64e1f05c3f19a7c963cb
Maurice Doherty <mauricedoherty@hotmail.com>
2023-05-10 15:34:13 +0200

    add client cart products

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,14 @@
-export default function About() {
-  return <div>About</div>;
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Image
+        src="/../public/Images/iphone-art-wallpapers-info.jpg"
+        alt="iphone art wallpaper example"
+        width={750}
+        height={550}
+      />
+    </div>
+  );
 }
--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Image
+        src="/../public/Images/iphone-art-wallpapers.jpg"
+        alt="iphone art wallpaper example"
+        width={750}
+        height={550}
+      />
+    </div>
+  );
 }
